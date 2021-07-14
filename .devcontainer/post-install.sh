25219e1e82d23840a5aa4b3bcc10dd56845dab08
johee <johchung@microsoft.com>
2021-07-13 19:20:11 -0700

    Replace LodeRunner with Web Validate (#1)

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # clone repos
-git clone https://github.com/retaildevcrews/loderunner
-pushd ./loderunner
+git clone https://github.com/retaildevcrews/webvalidate
+pushd ./webvalidate/src/app
 sed -i 's/api/zzz/' benchmark.json
 popd
 
